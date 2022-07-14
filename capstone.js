3b5c5c123e7c6806baf6f0729f46614c4e5b99ed
tparekh <tparekh8@gmail.com>
2022-07-13 21:48:26 -0400

    first commit

---
+++ capstone.js
@@ -0,0 +1,5 @@
+fetch('https://api.github.com/users/manishmshiva')
+    // Handle success
+    .then(response => response.json())  // convert to json
+    .then(json => console.log(json))    //print data to console
+    .catch(err => console.log('Request Failed', err)); // Catch errors
